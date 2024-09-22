007b5be9080497d62bc1ddeb31884502f87e4816
Robert Vari <mail.robertvari@gmail.com>
2024-09-22 15:54:07 +0200

    Resource loader

---
+++ py_components/resources.py
@@ -0,0 +1,16 @@
+from PySide6.QtCore import QObject, QUrl
+import os
+
+RESOURCES_PATH = os.path.dirname(__file__).replace("py_components", "resources")
+
+class Resources(QObject):
+    def get(self, file_name):
+        full_path = os.path.join(RESOURCES_PATH, file_name)
+        assert os.path.exists(full_path), f"Resouce path does noth exist: {full_path}"
+
+        return QUrl().fromLocalFile(full_path)
+    
+
+if __name__ == "__main__":
+    r = Resources()
+    print(r.get("poster.webp"))
