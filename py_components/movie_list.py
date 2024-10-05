c19a46b7ea1a75942b5713621727816d2b56a313
Robert Vari <mail.robertvari@gmail.com>
2024-10-05 14:54:57 +0200

    Movie List Worker

--- py_components/movie_list.py
+++ py_components/movie_list.py
@@ -1,5 +1,6 @@
-from PySide6.QtCore import QAbstractListModel, QModelIndex, Qt, QUrl
+from PySide6.QtCore import QAbstractListModel, QModelIndex, Qt, QObject, QRunnable, Signal, QThreadPool
 import tmdbsimple as tmdb
+import time
 
 tmdb.API_KEY = "83cbec0139273280b9a3f8ebc9e35ca9"
 tmdb.REQUESTS_TIMEOUT = 5
@@ -13,14 +14,33 @@ class MovieList(QAbstractListModel):
         super().__init__()
         self.__movies = []
 
-        self.fetch_movies()
+        # Create a jobpool for our worker
+        self.__job_pool = QThreadPool()
+        self.__job_pool.setMaxThreadCount(1)
 
-    def fetch_movies(self):
+        # Create our worker
+        self.__movie_list_worker = MovieListWorker()
+        
+        # Connect to its signals here
+        self.__movie_list_worker.signals.task_finished.connect(self.__insert_movie)
+
+        self.__fetch()
+
+    def __fetch(self):
+        # Start job pool
+        self.__job_pool.start(self.__movie_list_worker)
+    
+    def __insert_movie(self, movie_data):
+        print(f"Movie data: {movie_data}")
+
+    def fetch_movies_old(self):
+        print("Start fetching movies")
         movies = tmdb.Movies()
         popular_movies = movies.popular(page=1).get("results")
 
         for i in popular_movies:
             title = i.get("title")
+            print(f"Add movie: {title}")
             release_date = i.get("release_date")
             vote_average = int(round(i.get("vote_average") * 10))
             poster_path = f"{POSTER_TOOT}{i.get('poster_path')}"
@@ -42,3 +62,40 @@ class MovieList(QAbstractListModel):
         row = index.row()
         if role == MovieList.DataRole:
             return self.__movies[row]
+
+
+
+# Threading
+class WorkerSignals(QObject):
+    task_finished = Signal(dict)
+
+    def __init__(self):
+        super().__init__()
+
+class MovieListWorker(QRunnable):
+    def __init__(self):
+        super().__init__()
+        self.signals = WorkerSignals()
+        self.__movies = tmdb.Movies()
+
+    def run(self):
+        self.__fetch()
+
+    def __fetch(self):
+        popular_movies = self.__movies.popular(page=1).get("results")
+        
+        for i in popular_movies:
+            time.sleep(0.5)
+            title = i.get("title")
+            release_date = i.get("release_date")
+            vote_average = int(round(i.get("vote_average") * 10))
+            poster_path = f"{POSTER_TOOT}{i.get('poster_path')}"
+
+            movie_data = {
+                "title": title,
+                "release_date": release_date,
+                "vote_average": vote_average,
+                "poster_path": poster_path
+            }
+
+            self.signals.task_finished.emit(movie_data)
